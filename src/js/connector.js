cb890887142a5d9679d8f7fa729d3acdbb75f165
YRAbc <y.raton@abc-arbitrage.com>
2024-01-15 17:10:30 +0100

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,1 +1,6 @@
-console.log('Hola !!');
+// connector.js
+
+// Print a message to the console when the Power-Up is loaded
+console.log('Trello Power-Up Connector is loaded!');
+
+// Add any additional code or functionality here
