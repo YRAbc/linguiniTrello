c181d74ab060f5f24066328aba06b02dfe5af86c
YRAbc <y.raton@abc-arbitrage.com>
2024-01-16 17:47:49 +0100

    api rest co

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,34 @@ import Connector from './classes/connector/connector.js';
 
 console.log("Start Linguini");
 
-// Initialize TrelloPowerUp
-TrelloPowerUp.initialize({
-  'connector': function (t, options) {
-    const connector = new Connector();
-    connector.onPowerUpInit(t, options);
-    return connector;
-  },
-  // other Power-Up capabilities...
+var t = window.TrelloPowerUp.iframe({
+  appKey: 'fa9aab0039cce75efc142efc7e65a403',
+  appName: 'Linguini for Trello',
+  appAuthor: 'Yoann Raton'
 });
+
+// Initialize TrelloPowerUp
+window.TrelloPowerUp.initialize({
+  'card-buttons': function(t) {
+    return t.getRestApi()
+      .isAuthorized()
+      .then(function(isAuthorized) {
+        if (isAuthorized) {
+          return [{
+            text: 'David\'s Power-Up',
+            callback: showMenu
+          }];
+        } else {
+          return [{
+            text: 'David\'s Power-Up',
+            callback: showIframe
+          }];
+        }
+      });
+  }
+}, {
+  appKey: 'fa9aab0039cce75efc142efc7e65a403',
+  appName: 'Linguini for Trello',
+  appAuthor: 'Yoann Raton'
+});
+
