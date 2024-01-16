cdc3d87e5ccfeb0aef148385d76fbd7cd699acd8
YRAbc <y.raton@abc-arbitrage.com>
2024-01-16 17:20:21 +0100

    find workspace infos

--- src/js/classes/connector/connector.js
+++ src/js/classes/connector/connector.js
@@ -1,37 +1,39 @@
 // connector.js
+// Made by Yoann Raton, 16/01/2024
 
-// Print a message to the console when the Power-Up is loaded
-console.log('Trello Power-Up Connector is loaded!');
+// Import the Card class
+import Card from './card.js';
 
-// Trello API key
-const apiKey = 'fa9aab0039cce75efc142efc7e65a403';
-
-// Initialize Trello Power-Up
-window.TrelloPowerUp.initialize({
-  'board-buttons': function (t, options) {
-    return [{
-      icon: {
-        dark: 'https://linguini-trello.vercel.app/src/image/icon.jpg',
-        light: 'https://linguini-trello.vercel.app/src/image/icon.jpg'
-      },
-      text: 'Custom Button',
-      callback: function (t) {
-        // Your button functionality
-        console.log('Button clicked!');
-      }
-    }];
-  },
-  'card-buttons': function (t, options) {
-    return [{
-      icon: {
-        dark: 'https://linguini-trello.vercel.app/src/image/icon.jpg',
-        light: 'https://linguini-trello.vercel.app/src/image/icon.jpg'
-      },
-      text: 'Custom Card Button',
-      callback: function (t) {
-        // Your card button functionality
-        console.log('Card button clicked!');
-      }
-    }];
+export default class Connector {
+  constructor() {
+    this.boards = []; // Array to store information about boards
   }
-});
+
+  // This method is called when the Power-Up is initialized
+  onPowerUpInit(t, options) {
+    // Retrieve information about the workspace
+    const workspaceId = t.getContext().workspace;
+    console.log("Workspace ID:", workspaceId);
+
+    // Retrieve information about boards
+    t.lists('all')
+      .then((lists) => {
+        lists.forEach((list) => {
+          // Assuming each list corresponds to a board
+          const board = {
+            id: list.idBoard,
+            name: list.name,
+          };
+          this.boards.push(board);
+        });
+
+        // Log information about boards
+        console.log("Boards:", this.boards);
+      })
+      .catch((error) => {
+        console.error('Error fetching board information:', error);
+      });
+  }
+
+  // Add other methods and properties as needed
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,11 @@
-// Print a message to the console when the Power-Up is loaded
-console.log('hola!!!');
+// main.js
+// Made by Yoann Raton, 16/01/2024
+
+// Import the Connector class
+import Connector from './classes/connector/connector.js';
+
+// Create an instance of the Connector
+const connector = new Connector();
+
+// Call onPowerUpInit with the required Trello arguments
+connector.onPowerUpInit(t, options);
