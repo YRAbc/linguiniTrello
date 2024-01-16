f9fba7892705c93f506ae9032d6be9735698b6c7
YRAbc <y.raton@abc-arbitrage.com>
2024-01-16 16:34:01 +0100

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,10 +2,3 @@
 
 // Print a message to the console when the Power-Up is loaded
 console.log('Trello Power-Up Connector is loaded!');
-
-// Add any additional code or functionality here
-window.TrelloPowerUp.initialize({
-    'card-badges': function(t, opts) {
-        return[];
-    }
-})
