26480ec4c1dc3ffe83b4f1416b0e0c0be2039bb6
YRAbc <y.raton@abc-arbitrage.com>
2024-01-24 15:55:39 +0100

    debug

--- src/js/main.js
+++ src/js/main.js
@@ -19,67 +19,52 @@ window.TrelloPowerUp.initialize({
     'board-buttons': function (t, options) {
         return [
             {
+                // Remove the 'initPowerUp' flag check
                 text: 'Add',
                 callback: function (t) {
                     // Retrieve the board ID
                     let boardId = t.getContext().board;
-    
-                    // Check if the flag has been set
-                    t.get('board', 'private', 'initPowerUp')
-                        .then(initPowerUp => {
-                            if (!initPowerUp) {
-                                // Retrieve the board name using Trello API
-                                console.log("Update Linguini");
-                                t.board('name')
-                                    .then(function (board) {
-                                        const id = boardId;
-                                        const nam = board.name;
-    
-                                        if (id && nam) {
-                                            // add the board to the workspace
-                                            const boardObj = new Board(id, nam);
-    
-                                            // Use t to get information about lists on the board
-                                            t.lists('all')
-                                                .then(function (lists) {
-                                                    // Create a list object for each Trello list
-                                                    const listObjects = lists.map((list) => {
-                                                        const listObj = new List(list.id, list.name);
-    
-                                                        // For each card in the Trello list, create a Card object and add it to the list
-                                                        list.cards.forEach((card) => {
-                                                            const cardObj = new Card(card.id, card.name, list.id, list.name);
-                                                            listObj.addCard(cardObj);
-                                                        });
-    
-                                                        return listObj;
-                                                    });
-    
-                                                    // Set the lists for the board object
-                                                    boardObj.setLists(listObjects);
-                                                    opfwsp.addBoard(boardObj);
-    
-                                                    // Set the flag to indicate that the initialization has been done
-                                                    t.set('board', 'private', 'initPowerUp', true)
-                                                        .then(function () {
-                                                            console.log('Board added to workspace.');
-                                                        })
-                                                        .catch(function (error) {
-                                                            console.error('Error setting initPowerUp flag:', error);
-                                                        });
-                                                })
-                                                .catch(function (error) {
-                                                    console.error('Error fetching lists:', error);
-                                                });
-                                        }
+
+                    // Retrieve the board name using Trello API
+                    console.log("Update Linguini");
+                    t.board('name')
+                        .then(function (board) {
+                            const id = boardId;
+                            const nam = board.name;
+
+                            if (id && nam) {
+                                // add the board to the workspace
+                                const boardObj = new Board(id, nam);
+
+                                // Use t to get information about lists on the board
+                                t.lists('all')
+                                    .then(function (lists) {
+                                        // Create a list object for each Trello list
+                                        const listObjects = lists.map((list) => {
+                                            const listObj = new List(list.id, list.name);
+
+                                            // For each card in the Trello list, create a Card object and add it to the list
+                                            list.cards.forEach((card) => {
+                                                const cardObj = new Card(card.id, card.name, list.id, list.name);
+                                                listObj.addCard(cardObj);
+                                            });
+
+                                            return listObj;
+                                        });
+
+                                        // Set the lists for the board object
+                                        boardObj.setLists(listObjects);
+                                        opfwsp.addBoard(boardObj);
+
+                                        console.log('Board added to workspace.');
                                     })
                                     .catch(function (error) {
-                                        console.error('Error retrieving board information:', error);
+                                        console.error('Error fetching lists:', error);
                                     });
-                            } else {
-                                // Initialization has already been done
-                                console.log('Initialization has already been done.');
                             }
+                        })
+                        .catch(function (error) {
+                            console.error('Error retrieving board information:', error);
                         });
                 },
             },
