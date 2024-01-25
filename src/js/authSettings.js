899edac342b1e24937f86eca1d778cc6572005eb
YRAbc <y.raton@abc-arbitrage.com>
2024-01-25 12:21:10 +0100

    debug

--- src/js/authSettings.js
+++ src/js/authSettings.js
@@ -8,33 +8,54 @@ class oAuth {
 
   constructor() {
     // Trello API key and secret
-    this.apiKey = 'fa9aab0039cce75efc142efc7e65a403';
-    this.apiSecret = 'a54e7f3488ca0a442879d97a77b27993e7d6d3682ec10849cd099e8b8bbd9f16';
-    this.appName = 'Linguini';
-    this.authorizeCallback = 'https://linguini-trello.vercel.app/';
-    this.appAccessToken = "ATTA21dc26ef75ab9c9515db8d414d25dd4e68165a6760e8bd533a86c0a96070ab09DADAEE60"
+    this._apiKey = 'fa9aab0039cce75efc142efc7e65a403';
+    this._apiSecret = 'a54e7f3488ca0a442879d97a77b27993e7d6d3682ec10849cd099e8b8bbd9f16';
+    this._appName = 'Linguini';
+    this._authorizeCallback = 'https://linguini-trello.vercel.app/';
+    this._appAccessToken = "ATTA21dc26ef75ab9c9515db8d414d25dd4e68165a6760e8bd533a86c0a96070ab09DADAEE60"
   }
 
-    // Getters
-    get apiKey() {
-      return this.apiKey;
-    }
-  
-    get apiSecret() {
-      return this.apiSecret;
-    }
-  
-    get appName() {
-      return this.appName;
-    }
-  
-    get authorizeCallback() {
-      return this.authorizeCallback;
-    }
-  
-    get appAccessToken() {
-      return this.appAccessToken;
-    }
+  // Getters
+  get apiKey() {
+    return this._apiKey;
+  }
+
+  get apiSecret() {
+    return this._apiSecret;
+  }
+
+  get appName() {
+    return this._appName;
+  }
+
+  get authorizeCallback() {
+    return this._authorizeCallback;
+  }
+
+  get appAccessToken() {
+    return this._appAccessToken;
+  }
+
+  // Setters
+  set apiKey(value) {
+    this._apiKey = value;
+  }
+
+  set apiSecret(value) {
+    this._apiSecret = value;
+  }
+
+  set appName(value) {
+    this._appName = value;
+  }
+
+  set authorizeCallback(value) {
+    this._authorizeCallback = value;
+  }
+
+  set appAccessToken(value) {
+    this._appAccessToken = value;
+  }
 }
 
 export default oAuth;
