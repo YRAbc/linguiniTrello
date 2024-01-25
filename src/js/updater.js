8792286bc60e054e799d21f03e0372b71b624bb1
Yoann Raton <y.raton@abc-arbitrage.com>
2024-01-25 19:56:38 +0100

    debug

--- src/js/updater.js
+++ src/js/updater.js
@@ -60,7 +60,7 @@ class Updater {
                     else if (!existingInList) {
 
                         // Card has been moved
-                        const targetList = await this.getter.getListFromCard(card.card.getCardID());
+                        const targetList = await this.getter.getListFromCard(card.getCardID());
                         console.log(`Card with ID ${card.getCardID()}, (${card.getCardName()}) has been moved from ${list.getListName()} to ${targetList.getListName()}`);
                     }
 
