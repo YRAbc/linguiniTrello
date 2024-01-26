1b25c4bb86f56765466bdc29bbfbed9664cf10c5
Yoann Raton <y.raton@abc-arbitrage.com>
2024-01-26 18:05:49 +0100

    debugg

--- src/js/get.js
+++ src/js/get.js
@@ -183,7 +183,7 @@ class Get {
     }
   }
 
-  async getCustomFieldId(cardId, fieldName) {
+  async getCustomField(cardId, fieldName) {
     try {
       const response = await axios.get(
         `https://api.trello.com/1/cards/${cardId}/customFields?key=${this.oauth.apiKey}&token=${this.oauth.appAccessToken}`
@@ -192,7 +192,7 @@ class Get {
       const customFields = response.data;
       const matchingField = customFields.find(field => field.name === fieldName);
 
-      return matchingField.id;
+      return matchingField;
     } catch (error) {
       console.error(`Error getting custom fields:`, error.response ? error.response.data : error.message);
       throw error;
