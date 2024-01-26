c83748aeedbeb6706114167eec4c2d2e6a67ce58
Yoann Raton <y.raton@abc-arbitrage.com>
2024-01-26 15:53:06 +0100

    dropdown custom fields

--- src/js/post.js
+++ src/js/post.js
@@ -69,8 +69,6 @@ class Post {
 
         const customFields = customFieldsResponse.data;
 
-        console.log('Fetched custom fields data:', customFields);
-
         // Check if the custom field already exists
         const existingField = customFields.find(field => field.name === fieldName);
 
@@ -79,18 +77,14 @@ class Post {
             const customFieldId = existingField.id;
             await axios.put(
                 `https://api.trello.com/1/card/${cardId}/customField/${customFieldId}/item?key=${this.oauth.apiKey}&token=${this.oauth.appAccessToken}`,
-                { value: { text: fieldValue } }
+                { value: { option: { id: fieldValue } } } // Assuming fieldValue is the key of the selected dropdown option
             );
-
-            console.log(`Updated existing custom field ${fieldName} with value ${fieldValue}`);
         } else {
             // If the custom field doesn't exist, create it
             await axios.post(
                 `https://api.trello.com/1/card/${cardId}/customField/${fieldName}/item?key=${this.oauth.apiKey}&token=${this.oauth.appAccessToken}`,
-                { value: { text: fieldValue } }
+                { value: { option: { id: fieldValue } } }
             );
-
-            console.log(`Created new custom field ${fieldName} with value ${fieldValue}`);
         }
 
         console.log(`Card ${fieldName} updated successfully.`);
@@ -98,9 +92,7 @@ class Post {
         console.error(`Error updating card ${fieldName}:`, error.response ? error.response.data : error.message);
         throw error;
     }
-}
-
-
+  }
 
   async addOPFTechNumber(cardId, opfTechNumber) {
     try {
