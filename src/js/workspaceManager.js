60f5fe46d52a0729c7c48d23dce96664d4d7edb7
Yoann Raton <y.raton@abc-arbitrage.com>
2024-01-31 11:17:10 +0100

    debug

--- src/js/workspaceManager.js
+++ src/js/workspaceManager.js
@@ -215,14 +215,13 @@ class WorkspaceManager {
                                 console.log(`Card ID: ${boardCard.cardId}, OPFTech Number: ${cardOpfTechNumber}`);
                                 console.log(`Main Card ID: ${card.id}, OPFTech Number: ${opftechnumber}`);
 
-                                // Check if the card has the same OPFTech number  && card.id !== boardCard.cardId
-                                if (cardOpfTechNumber === opftechnumber) {
+                                // Check if the card has the same OPFTech number
+                                if (cardOpfTechNumber === opftechnumber && card.id !== boardCard.cardId) {
                                     console.log('Duplicate card found !!!!');
                                     // Get the json of the original card and update it
-                                    const json = JSON.stringify(card, null, 2);
-                                    await this.rqtInv.setJson(boardCard.cardId, json);
-
-                                    console.log('Duplicate card updated.');
+                                    //const json = JSON.stringify(card, null, 2);
+                                    //await this.rqtInv.setJson(boardCard.cardId, json);
+                                    //console.log('Duplicate card updated.');
                                 }
                             }
                         }
