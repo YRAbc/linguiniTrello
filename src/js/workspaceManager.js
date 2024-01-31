f0748cb7c6545c4b10df6329dbc00106686db582
Yoann Raton <y.raton@abc-arbitrage.com>
2024-01-31 11:06:45 +0100

    debug

--- src/js/workspaceManager.js
+++ src/js/workspaceManager.js
@@ -217,7 +217,7 @@ class WorkspaceManager {
 
                                 // Check if the card has the same OPFTech number
                                 if (cardOpfTechNumber === opftechnumber && card.cardId !== boardCard.cardId) {
-                                    console.log('Duplicate card found');
+                                    console.log('Duplicate card found !!!!');
                                     // Get the json of the original card and update it
                                     const json = JSON.stringify(card, null, 2);
                                     await this.rqtInv.setJson(boardCard.cardId, json);
