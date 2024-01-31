e2e3ea07c2e3891981dda4e4f22e21080c05fece
Yoann Raton <y.raton@abc-arbitrage.com>
2024-01-31 18:59:13 +0100

    debug

--- src/js/workspaceManager.js
+++ src/js/workspaceManager.js
@@ -91,7 +91,7 @@ class WorkspaceManager {
 
                             //  -> CARD MODIFIED
                             this.trellowsp.cardModifiedInListRule(card.getCardId(), list.getListId(), board.getBoardId());
-                            this.updateDuplicates(board.getBoardId(), existingInList);
+                            //this.updateDuplicates(board.getBoardId(), existingInList);
                             this.updateWorkspace();
                     } 
                     
