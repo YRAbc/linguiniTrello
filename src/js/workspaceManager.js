60ed1eed8e7ed73850c79d43b96b934f632195fa
Yoann Raton <y.raton@abc-arbitrage.com>
2024-02-02 18:16:23 +0100

    debug

--- src/js/workspaceManager.js
+++ src/js/workspaceManager.js
@@ -246,7 +246,7 @@ class WorkspaceManager {
                                                         const dupCardCustomFieldValueId = IdsConfigWorkspace.mappingCustOptionsIds(board.getBoardId(), mainCardCustomFieldItem.idValue);
                                                         console.log('Dup ' ,dupCardCustomFieldValueId);
 
-                                                        if (dupCardCustomFieldValueId) {
+                                                        if (dupCardCustomFieldValueId !== null) {
                                                             // Assuming customFieldOptionsId is defined elsewhere in your code
                                                             await this.rqtInv.setCustomField(duplicateCard.cardId, duplicateCardCustomFieldItem.id, dupCardCustomFieldValueId, "green");
                                                             console.log("Set custom field : ", duplicateCardCustomFieldItem.id, " on options : ", dupCardCustomFieldValueId, " for card : ", duplicateCard.cardId);
