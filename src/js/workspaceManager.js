ce1bf055fbd3506722a512d4209369f521135ea3
Yoann Raton <y.raton@abc-arbitrage.com>
2024-02-02 16:55:32 +0100

    debug

--- src/js/workspaceManager.js
+++ src/js/workspaceManager.js
@@ -224,8 +224,8 @@ class WorkspaceManager {
                                         //console.log(`Dup Card ID: ${duplicateCard.id}, name: ${duplicateCard.name}, OPFTech Number: ${cardOpfTechNumber}`);
                                         //console.log(`Main Card ID: ${mainCard.id}, name: ${mainCard.name}, OPFTech Number: ${opftechnumber}`);
                                         //const json = JSON.stringify(mainCard, null, 2);
-                                        //console.log('Board Card (duplicate) JSON:', JSON.stringify(duplicateCard, null, 2));
-                                        //console.log('Main Card JSON:', JSON.stringify(mainCard, null, 2));
+                                        console.log('Board Card (duplicate) JSON:', JSON.stringify(duplicateCard, null, 2));
+                                        console.log('Main Card JSON:', JSON.stringify(mainCard, null, 2));
     
                                         // Update Dup card
                                         await this.rqtInv.setCardUpdate(duplicateCard.id, mainCard);
