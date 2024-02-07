a9e4731cc87a502ed4d05a753b9e60868f3024bc
Yoann Raton <y.raton@abc-arbitrage.com>
2024-02-07 18:25:13 +0100

    best update

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,41 @@
+// main.js
+// Made by Yoann Raton, 16/01/2024
+
 // Import Workspace Manager
 import WorkspaceManager from './workspaceManager.js';
 
 // Start info
 console.log("Start Linguini For Trello Power Up");
 
-// Initialize Workspace Manager
-const workspaceManager = new WorkspaceManager();
+// update is made to see difference between current and opfwsp
+const wspmanaager = new WorkspaceManager();
+wspmanaager.updateWorkspace();
+setupPeriodicUpdates();
 
 // Initialize Trello Power-Up
 window.TrelloPowerUp.initialize({
-  'board-buttons': function(t, options){
-    return [{
-      text: 'Update',
-      callback: async function(t){
-        try {
-          // Check for modifications
-          await workspaceManager.checkForModifications();
-          t.alert({
-            message: 'Workspace updated successfully!',
-            duration: 5,
-            display: 'info'
-          });
-        } catch (error) {
-          console.error('Error updating workspace:', error);
-
-          // Show error message
-          t.alert({
-            message: 'Error updating workspace. Please try again later.',
-            duration: 5,
-            display: 'error'
-          });
-        }
-      }
-    }];
-  }
 });
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function setupPeriodicUpdates() {
+
+    setInterval(async () => {
+      try {
+        // Check for modifications
+        await wspmanaager.checkForModifications();
+      } catch (error) {
+        console.error('Error during periodic update:', error);
+
+        if (error.response && error.response.status === 429) {
+          // Rate limiting error
+          const retryAfter = error.response.headers['retry-after'] || 30; // Default to 30 seconds
+          console.log(`Rate limiting error. Retrying after ${retryAfter} seconds.`);
+          await sleep(retryAfter * 1000); // Convert seconds to milliseconds
+        }
+      }
+    }, 30000); // Update every 30 seconds
 }
+  
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
